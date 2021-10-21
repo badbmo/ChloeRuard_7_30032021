360107e6c4dbf02d1384767152f5b803fac3a912
Chloé R <clositi@live.fr>
2021-10-21 17:13:59 +0200

    prevent form submit

--- js/components/Search.js
+++ js/components/Search.js
@@ -1,8 +1,16 @@
 export default class Search {
 
+	workingSearch(){
+		document.addEventListener("submit", (e) => {
+			if (e.target.dataset.trigger === "form") {
+				e.preventDefault();
+			}
+		});
+	}
+
 	render() {
 		return `
-		<form>
+		<form data-trigger="form">
 		<div class="search">
 			<input type="search" class="search__input" placeholder="Rechercher un ingrédient, appareil, ustensiles ou une recette" name="search">
 			<input type="image" name="submit" class="search__submit" src="img/search_icon.svg" alt="search">
