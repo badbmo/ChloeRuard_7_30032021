6571af8db7da8939e64402016b6a57d3334e8cdd
Chloé R <clositi@live.fr>
2021-10-21 17:14:15 +0200

    sort with tags and searchbar functional

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import recipes from "./recipes.js";
 class Index {
 	constructor() {
 		this.recipes = recipes;
+		this.query="";
 		this.displayHeader();
 		this.displaySearch();
 		this.displayRecipeCard(this.recipes);
@@ -16,6 +17,7 @@ class Index {
 		this.displayList(this.recipes);
 		this.displayTag();
 		this.removeTag();
+		this.sortWithSearchBar();
 	}
 
 	displayHeader() {
@@ -28,6 +30,7 @@ class Index {
 		const searchContainer = document.querySelector(".searchContainer");
 		const search = new Search();
 		searchContainer.innerHTML += search.render();
+		search.workingSearch();
 	}
 
 	displayRecipeCard(recipes) {
@@ -68,7 +71,7 @@ class Index {
 				const tag = new Tag(type, content);
 				// type = define class, content = define data-id and content (see Tag.js)
 				tagContainer.innerHTML += tag.render();
-				this.sortRecipeTag();
+				this.sortRecipes();
 				this.emptyInputs();
 			}
 		});
@@ -84,55 +87,57 @@ class Index {
 			//delete tag if click on the div containing icon and span (text)
 			if (e.target.dataset.trigger === "tag") {
 				e.target.remove();
-				this.sortRecipeTag();
+				this.sortRecipes();
 			}
 			//delete tag if click on icon and span (text)
 			if (e.target.dataset.trigger === "tagDelete" || e.target.dataset.trigger === "tagContent") {
 				e.target.parentNode.remove();
-				this.sortRecipeTag();
+				this.sortRecipes();
 			}
 		});
 	}
 
-	sortRecipeTag() {
-		this.filteredRecipes = this.recipes;
+	sortRecipes(){
+		this.filteredRecipes=this.recipes;
+		console.log(this.query);
+		//search in name, description or ingredient if recipe contains searchstring from workingSearchBar
+		this.filteredRecipes = this.filteredRecipes.filter((recipe) => {
+			if (recipe.name.toLowerCase().includes(this.query)) {
+				return recipe;
+			}
+			if (recipe.description.toLowerCase().includes(this.query)) {
+				return recipe;
+			}
+			if (recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(this.query))) {
+				return recipe;
+			}
+		});
 		const tags = document.querySelectorAll(".tag");
 		tags.forEach((tag) => {
+			const contentTag = tag.dataset.id;
 			// if tag ingredient, filter all the recipes by this ingredient. return filteredRecipes filtered
 			if (tag.classList.contains("tag-ingredients")) {
-				const contentTag = tag.dataset.id;
 				this.filteredRecipes = this.filteredRecipes.filter((recipe) => {
 					return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(contentTag));
 				});
 			}
 			// if tag appliance, filter all the recipes by this apliance. return filteredRecipes filtered
 			if (tag.classList.contains("tag-appliance")) {
-				const contentTag = tag.dataset.id;
 				this.filteredRecipes = this.filteredRecipes.filter((recipe) => {
 					return recipe.appliance.toLowerCase().includes(contentTag);
 				});
 			}
 			// if tag ustensils, filter all the recipes by this ustensils. return filteredRecipes filtered
 			if (tag.classList.contains("tag-ustensils")) {
-				const contentTag = tag.dataset.id;
 				this.filteredRecipes = this.filteredRecipes.filter((recipe) => {
 					return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(contentTag));
 				});
 			}
 		});
-
 		this.displayRecipeCard(this.filteredRecipes);
 		this.displayList(this.filteredRecipes);
 		this.checkIfResultInTag();
-		// display message for "no result". remove message "no result" if results.
-		const messageContainer = document.querySelector(".messageContainer");
-		if (this.filteredRecipes.length == 0) {
-			messageContainer.innerHTML = `
-			<img src="./img/no_result.png" class="empty__image">
-			<span class="empty__message">Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc.</span>`;
-		} else {
-			messageContainer.innerHTML = "";
-		}
+		this.noResultMessage();
 	}
 
 	//if <li> clicked and transformed to tag, remove the <li> selected from the list of results (= a tag is unique)
@@ -145,6 +150,33 @@ class Index {
 		results.forEach((result) => {
 			if (tagsArray.includes(result.textContent.trim())) {
 				result.classList.add("tagged");
+			}
+		});
+	}
+
+	// display message for "no result". remove message "no result" if results.
+	noResultMessage() {
+		const messageContainer = document.querySelector(".messageContainer");
+		if (this.filteredRecipes.length == 0) {
+			messageContainer.innerHTML = `
+			<img src="./img/no_result.png" class="empty__image">
+			<span class="empty__message">Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc.</span>`;
+		} else {
+			messageContainer.innerHTML = "";
+		}
+	}
+
+	sortWithSearchBar() {
+		const searchInput = document.querySelector(".search__input");
+		searchInput.addEventListener("keyup", (e) => {
+			this.searchString = e.target.value.toLowerCase().trim();
+			if (this.searchString.length > 2) {
+				this.query=this.searchString;
+				this.sortRecipes();
+			}
+			if (this.searchString.length <= 2) {
+				this.query="";
+				this.sortRecipes();
 			}
 		});
 	}
